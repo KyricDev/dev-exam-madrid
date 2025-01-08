6fdf1c64df5458c497584c5715db53d887b66d3d
Ricky Dave Madrid <madridrickydave@gmail.com>
2025-01-08 18:41:47 +0800

    initial commit

---
+++ simple-auth/src/views/RegisterView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1>Register</h1>
+      <form>
+        <input type="text" />
+        <input type="submit" value="Send OTP" />
+      </form>
+    </div>
+  </template>
+  
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
