179cb12ced8c0f605e666d0eaf21ab594fc699e4
Ricky Dave Madrid <madridrickydave@gmail.com>
2025-01-08 19:36:06 +0800

    added basic phone number validation

--- simple-auth/src/views/RegisterView.vue
+++ simple-auth/src/views/RegisterView.vue
@@ -2,9 +2,58 @@
 import { ref } from 'vue'
 
 const validPhoneNumber = ref(false)
+const errorText = ref('')
+
 function validatePhoneNumber(e) {
-    console.log(e.target.value)
-    validPhoneNumber.value = !validPhoneNumber.value
+    let phoneNumber = e.target.value
+
+    if (phoneNumber[0] === '+') {
+        phoneNumber = phoneNumber.substring(1)
+
+        if (phoneNumber.substring(0, 2) !== '63') {
+            validPhoneNumber.value = false
+            errorText.value = 'Must Start With \'09\' or \'+63\''
+            return
+        }
+
+        if (phoneNumber.length !== 13) {
+            validPhoneNumber.value = false
+            errorText.value = 'Invalid Phone Number Length'
+            return
+        }
+
+        if (!/^\d+$/.test(phoneNumber)) {
+            validPhoneNumber.value = false
+            errorText.value = `Must Only Contain Numbers`
+            return
+        }
+        
+        validPhoneNumber.value = true
+        errorText.value = ''
+        return
+    }
+
+    if (phoneNumber.substring(0, 2) !== '09') {
+        validPhoneNumber.value = false
+        errorText.value = 'Must Start With \'09\' or \'+63\''
+        return
+    }
+
+    if (phoneNumber.length !== 11) {
+        validPhoneNumber.value = false
+        errorText.value = 'Invalid Phone Number Length'
+        return
+    }
+
+    if (!/^\d+$/.test(phoneNumber)) {
+        validPhoneNumber.value = false
+        errorText.value = `Must Only Contain Numbers`
+        return
+    }
+        
+    validPhoneNumber.value = true
+    errorText.value = ''
+    return
 }
 
 </script>
@@ -15,7 +64,10 @@ function validatePhoneNumber(e) {
       <form>
         <input type="text" class="text-input" @input="validatePhoneNumber" />
         <input type="submit" value="Send OTP"  :disabled="!validPhoneNumber" />
-    </form>
+      </form>
+      <p class="error-text">
+        {{ errorText }}
+      </p>
     </div>
   </template>
   
@@ -23,6 +75,10 @@ function validatePhoneNumber(e) {
 
   .text-input {
     border-radius: 5px;
+  }
+
+  .error-text {
+    color: red;
   }
   
   </style>
